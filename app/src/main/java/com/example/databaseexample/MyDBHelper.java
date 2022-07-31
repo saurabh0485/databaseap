fc1637d56827895014d4b620e56d3c0ca7f1834e
Saurabh <soukmr12@gmail.com>
2022-07-31 15:43:20 +0530

    database app

---
+++ app/src/main/java/com/example/databaseexample/MyDBHelper.java
@@ -0,0 +1,25 @@
+package com.example.databaseexample;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+
+
+    public MyDBHelper(Context context) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
